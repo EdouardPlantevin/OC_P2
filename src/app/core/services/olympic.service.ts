18220ac7cb2f4d3f51d21e7d6cd114cf370d53c5
EdouardPlantevin <plantevin.contact@gmail.com>
2025-08-12 11:02:08 +0200

    init chart homepage

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,11 +1,13 @@
 import {httpResource} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import {computed, Injectable} from '@angular/core';
 import {OlympicCountry} from "../models/Olympic";
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
+
+  // > Get Data
   private olympicUrl = './assets/mock/olympic.json';
 
   olympicsResource = httpResource<OlympicCountry[]>(() => ({
@@ -17,5 +19,33 @@ export class OlympicService {
     const olympics = this.olympicsResource.value();
     return olympics?.find(olympicCountry => olympicCountry.id == id);
   }
+  // < Get Data
+
+  // > Fonction
+  getFormattedOlympicCountriesPieChart(olympicsCountry: OlympicCountry[]) {
+    if (!olympicsCountry) {
+      return { countryName: [], totalMedals: [], countryId: [] };
+    }
+
+    // On crée un tableau intermédiaire avec les pays et leur total de médailles
+    let olympicsFormatted = olympicsCountry.map(olympic => {
+      return {
+        countryName: olympic.country,
+        totalMedals: olympic.participations.reduce((total, participation) => total + participation.medalsCount, 0),
+        countryId: olympic.id
+      };
+    });
+
+    // On trie par totalMedals décroissant
+    olympicsFormatted.sort((a, b) => b.totalMedals - a.totalMedals);
+
+    // On sépare en deux tableaux
+    return {
+      countryName: olympicsFormatted.map(item => item.countryName),
+      totalMedals: olympicsFormatted.map(item => item.totalMedals),
+      countryId: olympicsFormatted.map(item => item.countryId)
+    };
+  }
+  // < Fonction
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,32 +1,21 @@
 import {Component, computed, effect, inject, OnInit} from '@angular/core';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import {CardComponent} from "../../components/card/card.component";
+import {PieChartComponent} from "../../components/pie-chart/pie-chart.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   imports: [
-    CardComponent
+    CardComponent,
+    PieChartComponent
   ],
   standalone: true
 })
 export class HomeComponent {
-  // public olympics$: Observable<any> = of(null);
-  //
-  // constructor(private olympicService: OlympicService) {}
-  //
-  // ngOnInit(): void {
-  //   this.olympics$ = this.olympicService.getOlympics();
-  // }
 
   private olympicService: OlympicService = inject(OlympicService);
 
-  public olympics$ = computed(() => this.olympicService.olympicsResource.value() || []);
-
-  constructor() {
-    effect(() => {
-      console.log(this.olympics$());
-    });
-  }
+  public olympics = computed(() => this.olympicService.olympicsResource.value() || []);
 }
